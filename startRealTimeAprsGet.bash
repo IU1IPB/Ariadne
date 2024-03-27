91f11dde3b50366c9e3a67f2c5b3f6e511a1f34b
Ugo Poddine IU1IPB <159938850+IU1IPB@users.noreply.github.com>
2024-03-27 09:21:03 +0100

    Add files via upload

--- startRealTimeAprsGet.bash
+++ startRealTimeAprsGet.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd /home/pi/Ariadne
 source "/home/pi/Ariadne/ariadne.venv/bin/./activate"""
-python3 -u /home/pi/Ariadne/realTimeAprsGet.py &
+#python3 -u /home/pi/Ariadne/realTimeAprsGet.py &
+python3 -u /home/pi/Ariadne/realTimeAprsGet.py
